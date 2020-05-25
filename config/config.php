5f5b4fbe873c3f8c8c05e11c3d56e6de30ff08be
himanshubobade <himanshubobade007@gmail.com>
2020-05-26 04:01:25 +0530

    Master

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php 
+    define('ROOT_URL', 'http://localhost/phpsandbox/todo/');
+    define('DB_HOST','localhost');
+    define('DB_USER','root');
+    define('DB_PASS','12345');
+    define('DB_NAME','todo');
+
+?>
